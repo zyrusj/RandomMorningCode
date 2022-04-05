96d52e9b9e4725d712d7b457f94265745d4db2da
Zyrus <zyrusmmfj@gmail.com>
2022-04-05 08:11:53 -0400

    Add files via upload

---
+++ ListInsert.java
@@ -0,0 +1,18 @@
+package practice;
+
+
+/*Given two sorted integer lists, one has empty spaces and the number of empty spaces 
+ * are equal to the length of the second list. Insert the second list into the first
+ * ex: l1 = {1, 10, 20, _, _, _}; l2 = {5, 9, 30};
+ * l1 = {1, 10, 20, MAX.int, MAX.int, MAX.int};
+ * desired output = {1, 5, 9, 10, 20, 30}
+ */
+
+
+public class ListInsert {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
