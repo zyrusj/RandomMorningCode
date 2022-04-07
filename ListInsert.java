beff298420d52f2984a8698ff1129ae41734f024
Zyrus <zyrusmmfj@gmail.com>
2022-04-07 07:58:55 -0400

    Add files via upload

--- ListInsert.java
+++ ListInsert.java
@@ -1,5 +1,8 @@
 package practice;
 
+import java.util.ArrayList;
+
+
 
 /*Given two sorted integer lists, one has empty spaces and the number of empty spaces 
  * are equal to the length of the second list. Insert the second list into the first
@@ -8,11 +11,21 @@ package practice;
  * desired output = {1, 5, 9, 10, 20, 30}
  */
 
+/*
+ * Algorithm
+ * Binary search for insert index. 
+ * 
+ */
 
 public class ListInsert {
+	
+	public void binaryInsert(int val, ArrayList<int> list){
+		
+	}
 	
 	public static void main(String[] args) {
 		
 	}
-
+	
+	
 }
